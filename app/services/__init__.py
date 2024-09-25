96baa471c629cb2c32bc5d76ca9ba97e78f4998f
annisa <annisaputriwahyuni46@gmail.com>
2024-09-26 00:13:33 +0700

    chore: add file into gitignore

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,6 @@
 from .todo_service import TodoServ
 from .seed_service import SeedServ
-from .jwt_service import JWTService
 
-__all__ = ["TodoServ", "JWTService"]
+# from .jwt_service import JWTService
+
+__all__ = ["TodoServ", "SeedServ"]
