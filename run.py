9498c79e935a2486383d202507aae05d14caf801
annisa <annisaputriwahyuni46@gmail.com>
2024-09-25 22:31:01 +0700

    fix: change port

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # run.py
+import os
 from app import create_app
 
-app = create_app('development')
+app = create_app("development")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# PORT = os.environ.get("PORT", 5151)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5151, debug=True)
